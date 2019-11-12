f4c7c477d7d90e05f3b1466c9616a8d8cfab6c15
Vincent Tso <vincent11102@hotmail.com>
2019-11-12 15:35:30 +1100

    Rails Wikinimous completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+10.times do
+  verb = Faker::Verb.past
+  hipster = Faker::Hipster.word
+  name = Faker::FunnyName.name
+  Article.create(title: "The #{hipster} #{verb} of #{name}", content: Faker::Lorem.sentence )
+end
+
